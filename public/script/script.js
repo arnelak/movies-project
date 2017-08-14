cf91e26d00d4045a31a3e191ad1bf251c55f7168
unknown <arnelakovac@gmail.com>
2017-08-15 01:55:33 +0200

    First commit

--- public/script/script.js
+++ public/script/script.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
 
-    $(".fa").on('click',function () {
+    $(".searchIcon").on('click',function () {
         $("#myText").fadeToggle();
     });
 
+   // var docHeight = $(document).innerHeight();
+   // $(".backImg").css("height",docHeight+"px");
+
+
+    $(".navCollapse").on('click', function () {
+        $("header").toggleClass("active");
+    });
 });
