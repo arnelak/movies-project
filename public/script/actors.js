e50a6b630ba74206b2de8773081ad097ccff7e42
akeco <amerkecho@hotmail.com>
2017-08-31 00:09:05 +0200

    Actors tip

--- public/script/actors.js
+++ public/script/actors.js
@@ -1,4 +1,25 @@
 $(document).ready(function () {
+  var url = document.location.search.split("=");
+  var actorName = decodeURIComponent(url[1]);
 
+  /*
+    var actors = [
+      {
+        name: 'Brad pit',
+        biography: 'Tako i tako...',
+        movies: ['prvi', 'drugi', 'treci']
+      },
+      {
+        name: 'Angelina Joline',
+        biography: 'Tako i tako...',
+        movies: ['prvi', 'drugi', 'treci']
+      }
+    ]
+  */
+
+  // Pronaci kroz actors.forEach(...) da bude (item.name == actorName) i vratit taj item
+  // u njemu ces imat tacnog glumca
+
+  console.info(decoded);
 
 });
